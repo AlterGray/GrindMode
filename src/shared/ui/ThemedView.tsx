4451ff9d4e17a28204d9bc9369d992c74dda1458
Bohdan Datsenko <veryverymaily@gmail.com>
2025-05-08 08:43:29 +0300

    Enhance ThemedView component: extend props to include ViewProps for better flexibility and pass additional props to the underlying View.

--- src/shared/ui/ThemedView.tsx
+++ src/shared/ui/ThemedView.tsx
@@ -1,14 +1,19 @@
-import { View } from "react-native";
+import { View, ViewProps } from "react-native";
 
-export type ThemedViewProps = {
+export type ThemedViewProps = ViewProps & {
   className?: string;
   children?: React.ReactNode;
 };
 
-const ThemedView: React.FC<ThemedViewProps> = ({ children, className }) => {
+const ThemedView: React.FC<ThemedViewProps> = ({
+  children,
+  className = "",
+  ...props
+}) => {
   return (
     <View
       className={`bg-light-background dark:bg-dark-background ${className}`}
+      {...props}
     >
       {children ? children : null}
     </View>
