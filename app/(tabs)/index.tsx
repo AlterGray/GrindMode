fff4bc8555f7b07e64a9a935b44fb9e1b11ad9c7
Bohdan Datsenko <veryverymaily@gmail.com>
2025-05-08 16:27:28 +0300

    Allow showing "All routines folders" even there no routines

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
-import { useRouter } from "expo-router";
+import React from "react";
 import { useRoutineStore } from "@features/routine/routineStore";
 import ScrollTabs from "@shared/ui/ScrollTabs/ScrollTabs";
-import ConfirmDialog from "@shared/ui/ConfirmDialog";
-import ToggleOptions from "@shared/ui/ToggleOptions/ToggleOptions";
 import { useFolderStore } from "@features/folder/folderStore";
 import RoutineList from "@features/routine/RoutineList";
 
 // TODO
 const Index = () => {
-  const routines = useRoutineStore((state) => state.routines);
   const folders = useFolderStore((state) => state.folders);
 
   const isFoldersExists = folders.length > 1;
   const tabs = folders
     .map((folder) => {
-      if (
-        folder.id === "-1" &&
-        routines.filter((r) => r.folderId === folder.id).length === 0
-      )
-        return;
-      else if (folder.id === "-1")
-        return { title: "All", content: <RoutineList folderId={folder.id} /> };
+      if (folder.id === "-1" && folders.length == 1) return;
       else
         return {
           title: folder.name,
