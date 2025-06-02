54e01e05a81de0fca26ee7b6ffab58beb43b5ce5
Bohdan Datsenko <veryverymaily@gmail.com>
2025-06-02 14:27:13 +0300

    Add temporal fix for keyboard appearing when autoFocus is true

--- src/shared/hooks/useAutoFocus.ts
+++ src/shared/hooks/useAutoFocus.ts
@@ -2,13 +2,18 @@ import { useEffect } from "react";
 import { TextInput } from "react-native";
 
 // TODO consider to creating AutoFocusInput component to don't restrict ref type
-export const useAutoFocus = (ref: React.RefObject<TextInput | null>, autoFocus: boolean) => {
+// TODO autofocus worked no each time, probably for different devices we need different timeout
+// TODO think about another way to handle auto focus, maybe native code
+export const useAutoFocus = (
+  ref: React.RefObject<TextInput | null>,
+  autoFocus: boolean,
+) => {
   useEffect(() => {
     if (autoFocus && ref?.current) {
       const timeoutID = setTimeout(() => {
         ref.current?.focus();
-      }, 100);
+      }, 200);
       return () => clearTimeout(timeoutID);
     }
   }, [autoFocus]);
-}
+};
