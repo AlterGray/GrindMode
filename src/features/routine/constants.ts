671061b29349b75eb450c001f5eca532dc0bc4ed
Bohdan Datsenko <veryverymaily@gmail.com>
2025-06-15 12:18:11 +0300

    Add new property to RoutinePhaseMap in order to simplify calculating statistic

--- src/features/routine/constants.ts
+++ src/features/routine/constants.ts
@@ -6,27 +6,32 @@ export const RoutinePhaseMap: Record<
     label: string;
     from: number;
     to: number;
+    value: number;
   }
 > = {
   [RoutinePhase.Initiation]: {
     label: "Initiation",
     from: 0,
     to: 14,
+    value: 1,
   },
   [RoutinePhase.Consolidation]: {
     label: "Consolidation",
     from: 15,
     to: 60,
+    value: 2,
   },
   [RoutinePhase.Stabilization]: {
     label: "Stabilization",
     from: 61,
     to: 90,
+    value: 3,
   },
   [RoutinePhase.DeepIntegration]: {
     label: "Deep integration",
     from: 91,
     to: -1,
+    value: 4,
   },
 };
 
