33bd32f3ebe37c73afeb0b57256c459638289cef
Bohdan Datsenko <veryverymaily@gmail.com>
2025-06-17 10:38:56 +0300

    Improve styles for HorizontalBar

--- src/shared/ui/HorizontalTabBar.tsx
+++ src/shared/ui/HorizontalTabBar.tsx
@@ -15,14 +15,14 @@ const HorizontalTabBar: React.FC<HorizontalTabBarProps> = ({
 }) => {
   // ALLOW TO CHANGE TIME WITH SWIPE, ADD SWIPE-ANIMATION FOR ALL SWITCHES EVEN WITH TOUCH
   return (
-    <View className="p-1.5 gap-2 flex-row justify-around bg-light-backgroundSecondary dark:bg-dark-backgroundSurface">
+    <View className="p-1 gap-2 flex-row justify-around bg-light-backgroundSecondary dark:bg-dark-backgroundSurface">
       {tabs.map((tab, index) => (
         <Pressable
           key={tab.label}
           onPress={() => {
             onChange(index);
           }}
-          className={`p-3 flex-1 ${activeTab === index ? "bg-light-listItemBorder dark:bg-dark-selectedListItemBackground" : ""}`}
+          className={`p-2 flex-1 ${activeTab === index ? "bg-light-listItemBorder dark:bg-dark-selectedListItemBackground" : ""}`}
         >
           <ThemedText className="text-center font-medium">
             {tab.label}
