8fbcc1ad020eab0e13a7d9b86b5ae7ea8d86c03f
Bohdan Datsenko <veryverymaily@gmail.com>
2025-06-18 09:51:00 +0300

    Add reset statistic button to settings.tsx

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 
+import { useRitualStatisticStore } from "@features/rituals/statisticStore";
+
 import { useThemeStore } from "@shared/stores/themeStore";
+import { FloatingModalVariant } from "@shared/types/commonTypes";
+import { useGlobalFloatingModalStore } from "@shared/ui/GlobalFloatingModal/GlobalFloatingModalStore";
 import StyledButton from "@shared/ui/StyledButton";
 import ThemedView from "@shared/ui/ThemedView";
 
 const settings = () => {
   const { toggleTheme } = useThemeStore();
+  const statistics = useRitualStatisticStore((state) => state.statistics);
+  const openResetDialog = useGlobalFloatingModalStore(
+    (state) => state.openModal,
+  );
+  const removeStatistic = useRitualStatisticStore(
+    (state) => state.removeStatistic,
+  );
+
+  const handleResetStatistic = () => {
+    statistics.forEach((s) => removeStatistic(s.id));
+  };
+
+  const handleOpenResetDialog = () => {
+    openResetDialog({
+      title: "Reset Statistics",
+      variant: FloatingModalVariant.Danger,
+      onConfirm: handleResetStatistic,
+    });
+  };
 
   return (
-    <ThemedView className="flex-1 items-center justify-center">
+    <ThemedView className="flex-1 items-center justify-center gap-2">
       <StyledButton title="Switch theme" onPress={toggleTheme} />
+      <StyledButton
+        title="Reset Statistics"
+        variant="remove-contained-20"
+        onPress={handleOpenResetDialog}
+      />
     </ThemedView>
   );
 };
