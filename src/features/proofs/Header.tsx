20985d41f95161bec267bfe7b083b16e6ed37a97
Bohdan Datsenko <veryverymaily@gmail.com>
2025-07-22 14:02:56 +0300

    Implement quotes changing each 30 seconds

--- src/features/proofs/Header.tsx
+++ src/features/proofs/Header.tsx
@@ -1,17 +1,44 @@
+import { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 
 import AnimatedThemedText from "@shared/ui/ThemedText";
 
+import { quotes } from "./quotes";
+
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
+  const [activeQuoteIndex, setActiveQuoteIndex] = useState(0);
+  const intervalID = useRef(0);
+
+  useEffect(() => {
+    intervalID.current = setInterval(() => {
+      const newIndex = Math.floor(Math.random() * quotes.length);
+      setActiveQuoteIndex(newIndex);
+    }, 30000);
+
+    return () => clearInterval(intervalID.current);
+  }, []);
+
+  const quote = quotes[activeQuoteIndex].quote;
+  const author = quotes[activeQuoteIndex].author;
+
   return (
-    <View className="justify-center items-center">
+    <View className="justify-center items-center gap-2">
       <AnimatedThemedText className="text-3xl font-medium">
         Proof of Work
       </AnimatedThemedText>
-      <AnimatedThemedText className="text-lg font-medium" color="secondary">
-        "Discipline equals freedom."
+      <AnimatedThemedText
+        className="text-lg italic text-center leading-relaxed mb-1"
+        color="secondary"
+      >
+        "{quote}" -{" "}
+        <AnimatedThemedText
+          color="secondary"
+          className="text-base text-center font-light"
+        >
+          {author}
+        </AnimatedThemedText>
       </AnimatedThemedText>
     </View>
   );
